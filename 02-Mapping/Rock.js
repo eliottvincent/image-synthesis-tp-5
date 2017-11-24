4bf541872a39ba8482d3dcc72387a7ae8015c0cf
Eliott Vincent <eliott.20100@gmail.com>
2017-11-24 08:50:58 +0100

    02 Mapping, first sample

---
+++ 02-Mapping/Rock.js
@@ -0,0 +1,66 @@
+﻿// Définition de la classe Rock
+
+Requires("Mesh");
+Requires("MaterialRock");
+
+
+class Rock extends Mesh
+{
+    /** constructeur */
+    constructor()
+    {
+        // créer le matériau (this n'est pas encore défini)
+        let matrock  = new MaterialRock();
+
+        // initialisation de this
+        super("Rock", matrock);
+        this.m_MatRock = matrock;
+
+        // lire le fichier obj
+        this.loadObj("data/rock.obj", this.onRockLoaded);
+    }
+
+
+    /** appelée quand le maillage est chargé */
+    onRockLoaded()
+    {
+        // déterminer la boîte englobante du maillage
+        // TODO ce serait à déplacer dans la classe Mesh
+        let min = vec3.clone(this.m_VertexList[0].m_Coords);
+        let max = vec3.clone(this.m_VertexList[0].m_Coords);
+        for (let v of this.m_VertexList) {
+            vec3.min(min, min, v.m_Coords);
+            vec3.max(max, max, v.m_Coords);
+        }
+
+        // centre du cylindre dans le plan xz
+        let centre = vec3.create();
+        vec3.add(centre, min, max);
+        vec3.scale(centre, centre, 0.5);
+
+        // calcul des coordonnées de texture cylindriques
+        let coords = vec3.create();
+        for (let v of this.m_VertexList) {
+
+            /// TODO faire un calcul correct et pas n'importe quoi comme ceci (en fait, c'est un mapping plan)
+            let s = (v.m_Coords[0] - min[0]) * 0.25;
+            let t = (max[1] - v.m_Coords[1]) * 0.75;
+
+            // définition des coordonnées de texture
+            v.setTexCoords(s, t);
+        }
+    }
+
+
+    /**
+     * supprime toutes les ressources allouées dans le constructeur
+     */
+    destroy()
+    {
+        // méthode de la superclasse (suppression des VBOs)
+        super.destroy();
+
+        // supprimer le matériau
+        this.m_MatRock.destroy();
+    }
+}
---
+++ 02-Mapping/Scene.js
@@ -0,0 +1,117 @@
+﻿// Définition de la classe Scene
+
+// superclasses et classes nécessaires
+Requires("Rock");
+Requires("Grid");
+
+
+class Scene
+{
+    /** constructeur */
+    constructor()
+    {
+        // créer les objets à dessiner
+        this.m_Rock = new Rock();
+        this.m_Grid = new Grid();
+
+        // couleur du fond : gris très clair
+        gl.clearColor(0.9, 0.9, 0.9, 1.0);
+
+        // activer le depth buffer
+        gl.enable(gl.DEPTH_TEST);
+
+        // gestion souris
+        this.m_Azimut    = 240.0;
+        this.m_Elevation = 20.0;
+        this.m_Distance  = 6.0;
+        this.m_Clicked   = false;
+
+        // matrices
+        this.m_MatV = mat4.create();
+        this.m_MatVM = mat4.create();
+        this.m_MatTMP = mat4.create();
+    }
+
+
+    /**
+     * appelée quand on appuie sur une touche du clavier
+     * @param code : touche enfoncée
+     */
+    onKeyDown(code)
+    {
+        switch (code) {
+        case 'Z':
+            this.m_Distance *= Math.exp(-0.01);
+            break;
+        case 'S':
+            this.m_Distance *= Math.exp(+0.01);
+            break;
+        }
+    }
+
+    onMouseDown(btn, x, y)
+    {
+        this.m_Clicked = true;
+        this.m_MousePrecX = x;
+        this.m_MousePrecY = y;
+    }
+
+    onMouseUp(btn, x, y)
+    {
+        this.m_Clicked = false;
+    }
+
+    onMouseMove(x, y)
+    {
+        if (! this.m_Clicked) return;
+        this.m_Azimut  += (x - this.m_MousePrecX) * 0.2;
+        this.m_Elevation += (y - this.m_MousePrecY) * 0.2;
+        if (this.m_Elevation >  90.0) this.m_Elevation =  90.0;
+        if (this.m_Elevation < -90.0) this.m_Elevation = -90.0;
+        this.m_MousePrecX = x;
+        this.m_MousePrecY = y;
+    }
+
+
+    onSurfaceChanged(width, height)
+    {
+        // met en place le viewport
+        gl.viewport(0, 0, width, height);
+
+        // matrice de projection
+        this.m_MatP = mat4.create();
+        mat4.perspective(this.m_MatP, Utils.radians(12.0), width / height, 0.1, 30.0);
+    }
+
+
+    onDrawFrame()
+    {
+        // effacer l'écran
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+        // positionner la caméra
+        mat4.identity(this.m_MatV);
+
+        // éloignement de la scène
+        mat4.translate(this.m_MatV, this.m_MatV, vec3.fromValues(0.0, -0.25, -this.m_Distance));
+
+        // rotation demandée par la souris
+        mat4.rotateX(this.m_MatV, this.m_MatV, Utils.radians(this.m_Elevation));
+        mat4.rotateY(this.m_MatV, this.m_MatV, Utils.radians(this.m_Azimut));
+
+        // dessiner la grille
+        this.m_Grid.onDraw(this.m_MatP, this.m_MatV);
+
+        // dessiner le rocher
+        mat4.scale(this.m_MatV, this.m_MatV, vec3.fromValues(0.4, 0.4, 0.4));
+        this.m_Rock.onDraw(this.m_MatP, this.m_MatV);
+    }
+
+
+    /** supprime tous les objets de cette scène */
+    destroy()
+    {
+        this.m_Rock.destroy();
+        this.m_Grid.destroy();
+    }
+}
